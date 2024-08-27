da5af05ac4b6669d18cb16c669b7dcaadf4b56dd
joze01 <jose01.arteaga@gmail.com>
2024-08-27 13:32:55 -0600

    Add solution for the project

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,51 @@ interface TemperatureSummary {
   average: number
 }
 
-export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+const cityData: Map<string, Map<string, number[]>> = new Map()
+
+export function processReadings(readings: TemperatureReading[]) {
+  readings.forEach(({ time, temperature, city }) => {
+    const dateStr = time.toISOString().split('T')[0]
+
+    if (!cityData.has(city)) {
+      cityData.set(city, new Map())
+    }
+    const cityMap = cityData.get(city)
+    if (cityMap != null && cityMap != undefined) {
+      if (!cityMap.has(dateStr)) {
+        cityMap.set(dateStr, [])
+      }
+      cityMap.get(dateStr)!.push(temperature)
+    }
+  })
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  //add here your code
-  return null
+  const dateStr = date.toISOString().split('T')[0]
+  const cityMap = cityData.get(city)
+  if (!cityMap) {
+    return null
+  }
+  const temperatures = cityMap.get(dateStr)
+  if (!temperatures || temperatures.length === 0) {
+    return null
+  }
+
+  const first = temperatures[0]
+  const last = temperatures[temperatures.length - 1]
+  const high = Math.max(...temperatures)
+  const low = Math.min(...temperatures)
+  const average =
+    temperatures.reduce((sum, temp) => sum + temp, 0) / temperatures.length
+
+  return {
+    first,
+    last,
+    high,
+    low,
+    average,
+  }
 }
