6b7a621d9bfb4f09ecefe4755c983609418c89f8
marvnramos <marvinramos@ravn.co>
2024-11-20 10:27:08 -0600

    feat: implementing logic solution

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 // example interfaces that can be use
 // TIP: the types mentioned in the interfaces must be fulfilled in order to solve the problem.
 interface TemperatureReading {
@@ -13,8 +14,27 @@ interface TemperatureSummary {
   average: number
 }
 
+let result: Array<TemperatureReading>;
+
 export function processReadings(readings: TemperatureReading[]): void {
   // add here your code
+  result = readings;
+  let tempCity: string = "";
+  const mappedCitiesAndDates: Array<String[]> = [];
+  let alreadyMapped: boolean = false;
+
+  readings.forEach((reading) => {
+    if (!tempCity) tempCity = reading.city
+
+
+    mappedCitiesAndDates.forEach(item => {
+      if (item.includes(reading.city) && item.includes(reading.temperature.toString())) alreadyMapped = true
+    })
+    if (alreadyMapped && mappedCitiesAndDates.length !== 0) return
+    getTemperatureSummary(reading.time, reading.city)
+
+    mappedCitiesAndDates.push([reading.city, reading.time.toString()])
+  })
 }
 
 export function getTemperatureSummary(
@@ -22,5 +42,37 @@ export function getTemperatureSummary(
   city: string,
 ): TemperatureSummary | null {
   //add here your code
-  return null
+  const citiesArray = result.filter(
+    (value) =>
+      value.city === city &&
+      value.time.getTime() === date.getTime()
+  );
+
+  if (citiesArray.length === 0) return null;
+
+  const first = citiesArray[0].temperature;
+  const last = citiesArray[citiesArray.length - 1].temperature;
+  // const last = citiesArray.splice(-1)[0].temperature;
+  // const high = Math.max(...citiesArray.map((city) => city.temperature));
+  const high = citiesArray.reduce((acc, current) => {
+    if (!acc) acc = current.temperature;
+    if (acc === null || acc < current.temperature) acc = current.temperature;
+    return acc;
+  }, 0)
+  // const low = Math.min(...citiesArray.map((city) => city.temperature));
+  const low = citiesArray.reduce((acc, current) => {
+    if (!acc) acc = current.temperature;
+    if (acc === null || acc > current.temperature) acc = current.temperature;
+    return acc;
+  }, 0)
+  const total = citiesArray.reduce((acc, city) => acc + city.temperature, 0);
+  const average = total / citiesArray.length;
+
+  return {
+    first,
+    last,
+    high,
+    low,
+    average,
+  }
 }
