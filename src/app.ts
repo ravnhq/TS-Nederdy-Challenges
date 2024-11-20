688d507a3ccef53970ffc8adce0ee1bf99b4ae12
Javier Flores <jffc.dev@gmail.com>
2024-11-20 15:22:15 -0500

    feat: challenge 1 solved using for of

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,52 @@ interface TemperatureSummary {
   average: number
 }
 
+const globalReadings: TemperatureReading[] = []
+
 export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+  globalReadings.push(...readings)
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  //add here your code
-  return null
+  const summary: TemperatureSummary = {
+    first: 0,
+    last: 0,
+    high: 0,
+    low: 0,
+    average: 0,
+  }
+
+  const filteredReadings = globalReadings.filter(
+    (reading) =>
+      reading.city === city && reading.time.getTime() === date.getTime(),
+  )
+
+  if (!filteredReadings.length) {
+    return null
+  }
+
+  let index = 0
+  let temperatureAcc = 0
+  for (const reading of filteredReadings) {
+    temperatureAcc += reading.temperature
+    if (index === 0) {
+      summary.first = reading.temperature
+      summary.high = reading.temperature
+      summary.low = reading.temperature
+    } else if (index === filteredReadings.length - 1) {
+      summary.last = reading.temperature
+      summary.average = temperatureAcc / filteredReadings.length
+    }
+
+    summary.high =
+      summary.high > reading.temperature ? summary.high : reading.temperature
+    summary.low =
+      summary.low < reading.temperature ? summary.low : reading.temperature
+    index++
+  }
+
+  return summary
 }
