91a9a310c93dd6431182f80494c527baf7037b2f
Ezra Abarca <39611157+ezrillex@users.noreply.github.com>
2024-11-22 14:40:59 -0600

    feat(temperatures): summary and processing

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const groupBy = require('object.groupby')
+
 // example interfaces that can be use
 // TIP: the types mentioned in the interfaces must be fulfilled in order to solve the problem.
 interface TemperatureReading {
@@ -13,14 +16,41 @@ interface TemperatureSummary {
   average: number
 }
 
+// is dictionary of key values (key is a number and value is an array)
+let dailyReadings: { [date: string]: TemperatureReading[] }
+
 export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+  dailyReadings = groupBy(readings, (value: TemperatureReading) => {
+    return value.time.toDateString()
+  })
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
 ): TemperatureSummary | null {
-  //add here your code
-  return null
+  const readings = dailyReadings[date.toDateString()].filter((item) => {
+    return item.city === city
+  })
+  if (readings.length > 0) {
+    let avg = 0
+    let highest = 0
+    let lowest = Number.MAX_SAFE_INTEGER
+    for (const reading of readings) {
+      avg += reading.temperature
+      lowest = reading.temperature < lowest ? reading.temperature : lowest
+      highest = reading.temperature > highest ? reading.temperature : highest
+    }
+    avg /= readings.length
+
+    return {
+      average: avg,
+      first: readings[0].temperature,
+      high: highest,
+      last: readings[readings.length - 1].temperature,
+      low: lowest,
+    } as TemperatureSummary
+  } else {
+    return null
+  }
 }
