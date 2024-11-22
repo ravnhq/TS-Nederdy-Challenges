f259ab814ee41b195b39c2413c03088295aa319a
Victor Rodriguez <victormanuelrodriguez0899@gmail.com>
2024-11-22 13:52:17 -0600

    feat: add functions to process and summarize temperature readings

--- src/app.ts
+++ src/app.ts
@@ -13,14 +13,49 @@ interface TemperatureSummary {
   average: number
 }
 
+
+const redingMap: { [key: string]: { [key: string]: number[] } } = {}; 
+
 export function processReadings(readings: TemperatureReading[]): void {
-  // add here your code
+  readings.forEach((reding) => {
+
+    const date = reding.time.toString().split('T')[0];
+    if (!redingMap[date]) {
+      redingMap[date] = {};
+    }
+
+    if (!redingMap[date][reding.city]) {
+      redingMap[date][reding.city] = [];
+    }
+
+    redingMap[date][reding.city].push(reding.temperature);
+
+  });
 }
 
 export function getTemperatureSummary(
   date: Date,
   city: string,
+
 ): TemperatureSummary | null {
-  //add here your code
-  return null
+  const dateString = date.toString().split('T')[0];
+  
+  if (!redingMap[dateString] || !redingMap[dateString][city]) {
+    return null;
+  }
+
+  const temperatures = redingMap[dateString][city];
+  const first = temperatures[0];
+  const last = temperatures[temperatures.length - 1];
+  const high = Math.max(...temperatures);
+  const low = Math.min(...temperatures);
+  const average = temperatures.reduce((sum, temp) => sum + temp, 0) / temperatures.length;
+
+  return {
+    first,
+    last,
+    high,
+    low,
+    average,
+  };
 }
